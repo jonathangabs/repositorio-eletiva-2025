e1c2039e5ce484ce0a9c6f17a76bb68af1b85c3d
jonathangabs <endzs200@gmail.com>
2025-04-17 10:24:54 -0300

    Função Subtração

--- funcoes.php
+++ funcoes.php
@@ -1,7 +1,10 @@
 <?php
 
-function soma($num,$num2): mixed{
+function soma($num1,$num2): mixed{
     return($num1+$num2);
+}
+function subtracao($num1,$num2): mixed{
+    return($num1-$num2);
 }
 
 ?>
